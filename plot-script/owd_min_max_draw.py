346c794585dde53be9afbd42bfc3b037e7202766
MIN_ZZL <62577565+wlwl1011@users.noreply.github.com>
2023-08-22 15:15:04 +0900

    feat : Make tcp data to graph and intergrate to original graph

--- plot-script/owd_min_max_draw.py
+++ plot-script/owd_min_max_draw.py
@@ -1,6 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import colorsys
+from matplotlib import colors as pltcolors
+
+def darken_color(rgb_color, factor=0.6):
+    """
+    Darkens the given RGB color. The factor value is between 0 (completely dark) and 1 (no change).
+    """
+    r, g, b = rgb_color
+    h, l, s = colorsys.rgb_to_hls(r, g, b)
+    l *= factor
+    return colorsys.hls_to_rgb(h, l, s)
 
 # 1. Set the default style
 plt.style.use('default')
@@ -13,7 +24,7 @@ np.set_printoptions(precision=3, suppress=True)
 defualt = '/home/minzzl/networkCongestion/ns-allinone-3.33/ns-3.33/traces'
 st = ['loss']
 count_loss = [0, 1, 2, 3, 4, 5]
-protocol = ['QUIC']
+protocol = ['QUIC','TCP']
 cca = ['bbr', 'cubic', 'reno', 'vegas']
 
 owd_arr_QUIC = [ [[[0 for _ in range(10000)] for _ in range(5)] for _ in range(4)] for _ in range(6)]
@@ -21,6 +32,9 @@ owd_arr_TCP = [ [[[0 for _ in range(10000)] for _ in range(5)] for _ in range(4)
 
 min_owd_QUIC = np.zeros((4, 6))
 max_owd_QUIC = np.zeros((4, 6))
+min_owd_TCP = np.zeros((4, 6))
+max_owd_TCP = np.zeros((4, 6))
+
 
 data_points_QUIC = {algorithm: [] for algorithm in cca}
 
@@ -30,8 +44,8 @@ for standard in st:
         for pro in protocol:
             for cc in cca:
                 cc_i = cca.index(cc)
-                mins, maxs = [], []  # Temporary storage for the 5 min and max values
                 
+                mins, maxs = [], []  # Temporary storage for the 5 min and max values
                 for number in range(1, 5 + 1):  # 5번 실험한다
                     data = []
                     initial_port = 49153  # 10 임
@@ -40,11 +54,11 @@ for standard in st:
                     if pro == 'QUIC':
                         initial_port = 49153
                         initial_server_port = 1234
-                        for i in range(10):
+                        for i in range(5):
                             file_path = defualt + '/{}/{}/{}/{}/{}/1_10.1.1.1:{}_10.1.3.2:{}_owd.txt'.format(standard, count, pro, cc, number, initial_port, initial_server_port)
                             if os.path.isfile(file_path):
                                 if os.stat(file_path).st_size == 0:
-                                    print(name, '파일이 비어있습니다.')
+                                    print('파일이 비어있습니다.')
                                 else:
                                     with open(file_path, 'r') as f:
                                         for line in f:
@@ -54,96 +68,138 @@ for standard in st:
                             else:
                                 print(file_path, "파일이 없습니다.")
                             initial_port += 1
-                            initial_server_port += 1
-                             
-                    if data:
-                        #print(data)
-                        mins.append(min(data))
-                        maxs.append(max(data))
-                    else:
-                        print('oh no! There isnt data!',d_i, cc,number)    
+                            #initial_server_port += 1
+                        initial_port = 49153    
+                        # for i in range(5):
+                        #     file_path = defualt + '/{}/{}/{}/{}/{}/1_10.1.4.2:{}_10.1.5.2:{}_owd.txt'.format(standard, count, pro, cc, number, initial_port, initial_server_port)
+                        #     if os.path.isfile(file_path):
+                        #         if os.stat(file_path).st_size == 0:
+                        #             print(file_path,'파일이 비어있습니다.')
+                        #         else:
+                        #             with open(file_path, 'r') as f:
+                        #                 for line in f:
+                        #                     cols = line.strip().split()
+                        #                     if cols and len(cols) == 4:
+                        #                         data.append(int(cols[2]))  # 시간이고 4
+                        #     else:
+                        #         print(file_path, "파일이 없습니다.")
+                        #     initial_port += 1
+                            #initial_server_port += 1    
                     
-                    # else:
-                    #     # TCP
-                    #     initial_port = 49153
-                    #     initial_server_port = 1234
-                    #     for i in range(20, 30):
-                    #         file_path = defualt + '/{}/{}/{}/{}/{}/10.1.1.1_{}_10.1.3.2_{}_rtt.txt'.format(standard, count, pro, cc, number, initial_port, initial_server_port)
-                    #         if os.path.isfile(file_path):
-                    #             if os.stat(file_path).st_size == 0:
-                    #                 print(name, '파일이 비어있습니다.')
-                    #             else:
-                    #                 with open(file_path, 'r') as f:
-                    #                     for line in f:
-                    #                         cols = line.strip().split()
-                    #                         if cols and len(cols) == 2:
-                    #                             data.append(int(cols[1]))
-                    #         else:
-                    #             print(file_path, "파일이 없습니다.")
-                    #         initial_port += 1
-                    #         initial_server_port += 1
+                    else:
+                        # TCP
+                        initial_port = 49153
+                        initial_server_port = 5000
+                        for i in range(5):
+                            file_path = defualt + '/{}/{}/{}/{}/{}/10.1.1.1_{}_10.1.3.2_{}_rtt.txt'.format(standard, count, pro, cc, number, initial_port, initial_server_port)
+                            if os.path.isfile(file_path):
+                                if os.stat(file_path).st_size == 0:
+                                    print(name, '파일이 비어있습니다.')
+                                else:
+                                    with open(file_path, 'r') as f:
+                                        for line in f:
+                                            cols = line.strip().split()
+                                            if cols and len(cols) == 2:
+                                                data.append(int(cols[1]))
+                            else:
+                                print(file_path, "파일이 없습니다.")
+                            initial_port += 1
+                        #initial_server_port += 1
                  
-                 # Storing the average of the 5 min values and 5 max values        
+                 # Storing the average of the 5 min values and 5 max values   
+                if data:
+                    #print(data)
+                    mins.append(min(data))
+                    maxs.append(max(data))      
+                else:
+                    print('oh no! There isnt data!',d_i, cc,number)    
+
                 if pro == 'QUIC':
+                
                     min_owd_QUIC[cc_i][d_i] = sum(mins) / len(mins) if mins else 0
                     max_owd_QUIC[cc_i][d_i] = sum(maxs) / len(maxs) if maxs else 0   
                     # print(cc,d_i,min_owd_QUIC[cc_i][d_i])     
-                    # print(cc,d_i,max_owd_QUIC[cc_i][d_i])    
+                    # print(cc,d_i,max_owd_QUIC[cc_i][d_i])   
+                else:
+                    min_owd_TCP[cc_i][d_i] = sum(mins) / len(mins) if mins else 0
+                    max_owd_TCP[cc_i][d_i] = sum(maxs) / len(maxs) if maxs else 0  
+                    # min_owd_TCP[cc_i][d_i] = 100
+                    # max_owd_TCP[cc_i][d_i] = 800
+
+
                         
 
 # 3. Draw a graph representing the range of OWDs for all congestion control algorithms according to the error rate
-fig, ax = plt.subplots(figsize=(12, 8))
+fig, ax = plt.subplots(figsize=(15, 10))
 
-# We will set up data for each congestion control algorithm
-box_data_by_cc = {algorithm: [] for algorithm in cca}
-
-for cc_idx, cc_label in enumerate(cca):
-    for error_rate_idx in range(len(count_loss)):
-        min_val = min_owd_QUIC[cc_idx][error_rate_idx]
-        max_val = max_owd_QUIC[cc_idx][error_rate_idx]
-        box_data_by_cc[cc_label].append([min_val, max_val])
-
-# We will plot each congestion control algorithm's data side-by-side for each error rate
-positions = []
 all_data = []
 
-for idx, error_rate in enumerate(count_loss):
-    current_data_group = []
+# Iterate over each error rate
+for error_rate_idx in range(len(count_loss)):
+    current_error_data = []
+    
+    # For TCP first
     for cc_idx, cc_label in enumerate(cca):
-        box_data = box_data_by_cc[cc_label][idx]
-        current_data_group.append(box_data)
-    all_data.append(current_data_group)
+        # Get data for TCP
+        min_val_tcp = min_owd_TCP[cc_idx][error_rate_idx]
+        max_val_tcp = max_owd_TCP[cc_idx][error_rate_idx]
+        current_error_data.append([min_val_tcp, max_val_tcp])
+        
+    # Then for QUIC
+    for cc_idx, cc_label in enumerate(cca):
+        # Get data for QUIC
+        min_val_quic = min_owd_QUIC[cc_idx][error_rate_idx]
+        max_val_quic = max_owd_QUIC[cc_idx][error_rate_idx]
+        current_error_data.append([min_val_quic, max_val_quic])
+    
+    all_data.extend(current_error_data)  # Add the data for the current error rate
 
-# Define colors
+group_width = 2 * len(cca)
+positions = [i + j * (group_width + 1) for j in range(len(count_loss)) for i in range(group_width)] 
+
+bp = ax.boxplot(all_data, positions=positions, patch_artist=True)
+
+# Set the x-tick labels and centers
+xtick_centers = [(group_width) * i + group_width / 2 for i in range(len(count_loss))]
+ax.set_xticks(xtick_centers)
+
+# Set colors
 colors = {
     'bbr': 'pink',
     'cubic': 'gray',
-    'vegas': 'brown',
+    'vegas': 'purple',
     'reno': 'skyblue'
 }
 
-# Define positions for each box
-group_width = len(cca)
-positions = [i * (group_width + 1) + j for i in range(len(count_loss)) for j in range(group_width)]
-bp = ax.boxplot([item for sublist in box_data_by_cc.values() for item in sublist], positions=positions, patch_artist=True)
-
-# Set colors
+count = 0
 for i, box in enumerate(bp['boxes']):
+    # Determine the congestion control algorithm
     cc_label = cca[i % len(cca)]
-    box.set_facecolor(colors[cc_label])
+    color = colors[cc_label]
 
-# Adjust x-ticks' positions to be in the center of each group of four boxes
-ax.set_xticks([(group_width + 1) * i + group_width / 2 for i in range(len(count_loss))])
+    # Determine if it's for TCP
+    if count < 4:
+        #print(count,color)
+        color = darken_color(pltcolors.to_rgb(color))
+    
+    box.set_facecolor(color)
+    count += 1
+    if count >= 8:
+        count = 0
+   
+    
+
 ax.set_xticklabels(count_loss)
 ax.set_xlabel('Loss Count')
 ax.set_ylabel('One-Way Delay')
 ax.set_title('OWD Range for Each Congestion Control Algorithm in QUIC Grouped by Loss Count')
 
 # Legend based on colors
+# Legend based on colors
 from matplotlib.patches import Patch
-legend_elements = [Patch(facecolor=colors[cc], label=cc) for cc in cca]
+legend_elements = [Patch(facecolor=darken_color(pltcolors.to_rgb(colors[cc])), label=f"{cc} TCP") for cc in cca] + \
+                  [Patch(facecolor=colors[cc], label=cc) for cc in cca]
 ax.legend(handles=legend_elements, loc='upper left', bbox_to_anchor=(1, 1))
-
 
 ax.grid(axis='y', linestyle='--', linewidth=0.7, alpha=0.6)
 
